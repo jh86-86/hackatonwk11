f21afa4c764bec384d839970777dd686ca9a7898
GeorgeC <collingridgegeorge@gmail.com>
2021-03-19 16:26:56 +0000

    merged

--- frontend/src/components/GenrePage/Pop/index.js
+++ frontend/src/components/GenrePage/Pop/index.js
@@ -1,23 +1,33 @@
-
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+function Pop() {
+  const { userOne, isAuthenticated, isLoading } = useAuth0();
+  const [popMusic, setPopMusic] = useState("");
 
-function Pop(){
-    const { userOne, isAuthenticated, isLoading } = useAuth0();
+  useEffect(() => {
+    FetchPop();
+  }, []);
 
-    if (isLoading) {
-      return <div>Loading ...</div>;
-    }
-      
-      return (
-          isAuthenticated && (
-        <div>
-            <h1>Pop</h1>
-            
-        </div>
+  async function FetchPop() {
+    let res = await fetch("http://localhost:5000/MusicApp");
+    let data = await res.json();
+    console.log(data[0].youTubeLink);
+    setPopMusic(data[0].youTubeLink);
+  }
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <h1>Pop</h1>
+        <video url={popMusic} />
+      </div>
     )
-      );
+  );
 }
 
 export default Pop;
